3b919446bad2592206cb05b0eae69031b560b37a
Ivan Yurovich <yourovichivan@gmail.com>
2019-01-16 10:09:59 +0200

    added wait() to userMessages

--- Android/SocketServer/src/MessageManager.java
+++ Android/SocketServer/src/MessageManager.java
@@ -20,12 +20,12 @@ public class MessageManager implements Runnable{
         while (loopFlag){
             try {
                 synchronized (userMessages) {
-//                    try {
-//                        userMessages.wait();
-//                    } catch (InterruptedException e) {
-//                        e.printStackTrace();
-//                    }
-//                    System.out.println("Reader notified");
+                    try {
+                        userMessages.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Reader notified");
                     while (userMessages.size() > 0) {
                         String msg = userMessages.get(0).rawMessage;
                         userMessages.remove(0);
