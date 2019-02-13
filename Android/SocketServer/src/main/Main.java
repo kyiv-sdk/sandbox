c05c92ddfbdd2b6f35d043a58a2f3f0b9921d047
Ivan Yurovich <yourovichivan@gmail.com>
2019-02-13 14:23:08 +0200

    Moved port numbers to constant fields

--- Android/SocketServer/src/main/Main.java
+++ Android/SocketServer/src/main/Main.java
@@ -4,12 +4,15 @@ import main.basic_server.BasicServer;
 import main.ssl_server.SSL_Server;
 
 public class Main {
+    private static final int BASIC_PORT = 4545;
+    private static final int SSL_PORT = 5454;
+
     public static void main(String[] args) {
-        BasicServer basicServer = new BasicServer(4545);
+        BasicServer basicServer = new BasicServer(BASIC_PORT);
         Thread basicServerThread = new Thread(basicServer);
         basicServerThread.start();
 
-        SSL_Server ssl_server = new SSL_Server(5454);
+        SSL_Server ssl_server = new SSL_Server(SSL_PORT);
         Thread SSLServerThread = new Thread(ssl_server);
         SSLServerThread.start();
     }
